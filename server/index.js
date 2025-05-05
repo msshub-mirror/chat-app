70616339d36bfc2f846343b61018cdad15bf2867
msshub-mirror <zakuro877.5@gmail.com>
2025-05-06 01:01:03 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const { Pool } = require('pg')
 const cors = require('cors')
 
